c50c941232176eb4f421ca7ac1c3a760942c6c76
xwy <106575654+eiheixwy@users.noreply.github.com>
2022-12-07 15:43:49 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,55 @@
+# Yunzai插件
+
+#### 介绍
+
+闲暇时整出的yunzai插件，依赖云崽实现功能。仅适配V3。
+
+由衷感谢 渔火 佬的帮助
+
+#### 插件功能
+
+精简随机st：随机一张较新的美图（bushi）
+
+setuplus:随机一张较为古老的美图（bushi）
+
+注：默认st为非18（可更改，支持热更新），setuplus为18（不可更改），群聊慎用！！！
+
+60s后撤回（可更改），支持屏蔽群。（支持热更新）
+
+（代码片段极其简单，可自行更改）
+
+#### 食用方法
+
+发送#cv帮助  即可查看插件菜单
+
+精简随机st：st、#st
+
+setuplus：setuplus、#setuplus
+
+
+#### 安装方法
+
+推荐使用gitee安装（需要重启）
+
+在云崽目录打开终端，运行
+
+```
+git clone https://gitee.com/xwy231321/yunzai-c-v-plugin.git ./plugins/yunzai-c-v-plugin/
+
+```
+
+也可以单独下载setuplus或精简随机st，自行安装至Yunzai/plugins/example目录下
+
+暂时没整单独更新指令（应该很～久都不会更新，啥时候会了啥时候加上）
+
+未适配锅巴（应该用不到吧，不会整不会整，诶嘿）
+
+#### 反馈
+
+QQ：2060403379（小白，大佬们喷轻点）
+
+#### 其他
+
+[云崽bot](https://gitee.com/Le-niao/Yunzai-Bot)
+
+[Yunzai-Bot插件索引](https://gitee.com/Hikari666/Yunzai-Bot-plugins-index)
---
+++ 精简随机st.js
@@ -0,0 +1,39 @@
+//项目地址https://gitee.com/xwy231321/yunzai-c-v-plugin
+//小白，大佬们喷轻点
+//感谢渔火佬的帮助
+//推荐：云崽插件库：https://gitee.com/yhArcadia/Yunzai-Bot-plugins-index （gitee）   https://github.com/yhArcadia/Yunzai-Bot-plugins-index  （github）
+import { segment } from "oicq";
+import fetch from "node-fetch";
+import plugin from '../../lib/plugins/plugin.js';
+
+let blacklist = [] //黑名单群，多个qq群时用逗号(英文)隔开，例：10000,10001
+
+export class biyingsetu extends plugin {
+    constructor() {
+        super({
+            name: '轻凉图',
+            dsc: 'st',
+            event: 'message',
+            priority: 1,//优先级，越低优先度越高
+            rule: [
+                {
+                    reg: '^#?st(.*)$',
+                    fnc: 'setu'
+                }
+            ]
+        })
+    }
+    async setu(e) {
+        let url = "https://api.lolicon.app/setu/v2?r18=0"//←r18=0为非18，1有18，2为混合（不绝对）详情见api地址：https://api.lolicon.app/#/setu
+        let response = await fetch(url); 
+    //调用接口获取数据
+    let obj = await response.json(); 
+    //数据转换为obj
+    console.log(obj)
+    //打印obj，得到obj.data[0].urls.original
+    e.reply([segment.image(obj.data[0].urls.original)],false,{recallMsg:60})//←此处recallMsg后为撤回时间，单位秒，默认60s若为0则不撤回
+    //把得到的obj.data[0].urls.original以图片形式发送
+        return true
+    }
+}
+
