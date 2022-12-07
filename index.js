1ad837ae27a4c65057ad8f881f62136af22abe76
xwy <8921883+xwy231321@user.noreply.gitee.com>
2022-12-07 15:51:16 +0000

    update
    Signed-off-by: xwy <8921883+xwy231321@user.noreply.gitee.com>

---
+++ index.js
@@ -0,0 +1,29 @@
+import fs from 'node:fs'
+
+const files = fs.readdirSync('./plugins/yunzai-c-v-plugin/apps').filter(file => file.endsWith('.js'))
+
+let ret = []
+
+logger.info('------（-＾〇＾-）-----')
+logger.info('清凉图插件极速加载中～')
+logger.info('-----(/^▽^)/------')
+
+
+files.forEach((file) => {
+  ret.push(import(`./apps/${file}`))
+})
+
+ret = await Promise.allSettled(ret)
+
+let apps = {}
+for (let i in files) {
+  let name = files[i].replace('.js', '')
+
+  if (ret[i].status != 'fulfilled') {
+    logger.error(`载入插件错误：${logger.red(name)}`)
+    logger.error(ret[i].reason)
+    continue
+  }
+  apps[name] = ret[i].value[Object.keys(ret[i].value)[0]]
+}
+export { apps }
