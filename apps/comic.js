bbbdc4b1dfa6419fdba9ad7d2308b3c46a3417ea
xwy <8921883+xwy231321@user.noreply.gitee.com>
2022-12-08 11:09:27 +0000

    update
    Signed-off-by: xwy <8921883+xwy231321@user.noreply.gitee.com>

---
+++ apps/comic.js
@@ -0,0 +1,29 @@
+import { segment } from "oicq";
+import fetch from "node-fetch";
+import plugin from '../../../lib/plugins/plugin.js';
+
+export class biyingsetu extends plugin {
+    constructor() {
+        super({
+            name: 'B站404随机小漫画',
+            dsc: '随机漫画',
+            event: 'message',
+            priority: 4999,//优先级，数越小优先度越高
+            rule: [
+                {
+                    reg: '^#?随机漫画(.*)$',
+                    fnc: 'comic'
+                }
+            ]
+        })
+    }
+    async comic(e) {
+        let url = `https://www.acy.moe/api/404`
+        await e.reply('我去给你找啦，稍等哦～',true,{recallMsg:7})
+        let msg=[segment.image(url)]
+        e.reply(msg,false)
+        return true                           
+    }
+}
+
+
