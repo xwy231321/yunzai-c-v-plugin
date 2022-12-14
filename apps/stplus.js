f034c7554bfee306c21819cfa5cab99179b1ff37
xwy <8921883+xwy231321@user.noreply.gitee.com>
2022-12-14 04:08:34 +0000

    update apps/stplus.js.
    Signed-off-by: xwy <8921883+xwy231321@user.noreply.gitee.com>

--- apps/stplus.js
+++ apps/stplus.js
@@ -36,7 +36,7 @@ export class stplus extends plugin {
         }
         let url = `https://www.acy.moe/api/r18`
         await e.reply('正在给你找涩涩的图片啦～',true,{recallMsg:7})
-        let msg = [segment.image(url)]
+        let msg = [segment.flash(url)]
         let abc =  await e.reply(msg,false,{recallMsg:60})//←此处recallMsg后为撤回时间，单位秒，默认60秒，时间为0则不撤回
         if (!abc) return e.reply('好、好铯(//// ^ ////)……被、被吞啦o(≧口≦)o',true,{recallMsg:60})
        return true
