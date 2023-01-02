d7ecc15e647830f3680ca9ce864d4ad91734c5c6
xwy <2060403379@qq.com>
2023-01-02 16:39:37 +0800

    更新云溪图链接源

--- apps/yxysj.js
+++ apps/yxysj.js
@@ -43,7 +43,7 @@ export class stplus extends plugin {
         if (ismaster) {
         if(!e.isMaster) return false
         }
-        let url = `http://47.100.2.140`
+        let url = `http://47.100.2.140/yxy.php`
         await e.reply('正在给你找云溪院的图片啦～',true,{recallMsg:7})
         let image = []
         let num = 1
@@ -72,7 +72,7 @@ export class stplus extends plugin {
         if(!e.isMaster) 
         return false
         }
-        let url = `http://47.100.2.140`
+        let url = `http://47.100.2.140/yxy.php`
         await e.reply('正在给你找云溪院的图片啦～',true,{recallMsg:7})
         let num = e.msg.match(/\d+/)
         let image = []
