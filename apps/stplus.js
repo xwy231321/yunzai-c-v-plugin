1145b80288e49e21484a5d54bac9edca177c50ae
xwy <2060403379@qq.com>
2023-01-05 18:03:36 +0800

    更改 铯图 失效源

--- apps/stplus.js
+++ apps/stplus.js
@@ -45,7 +45,7 @@ export class stplus extends plugin {
             if(!e.isMaster) return false
         }
         if (e.isGroup) {
-        let url = `https://www.acy.moe/api/r18`
+        let url = `http://api.xemxem.ml/API/setu.php`
             let image = []
             let num = 1
         await e.reply('正在给你找涩涩的图片啦～',true,{recallMsg:7})
@@ -55,7 +55,7 @@ export class stplus extends plugin {
         if (!abc) return e.reply('好、好铯(//// ^ ////)……被、被吞啦o(≧口≦)o',true,{recallMsg:60})
         return true;
       }
-      let url = `https://www.acy.moe/api/r18`
+      let url = `http://api.xemxem.ml/API/setu.php`
         await e.reply('正在给你找涩涩的图片啦～',true,{recallMsg:7})
         let msg = [segment.image(url)]
         let abc =  await e.reply(msg,false,{recallMsg:0})//私聊撤回间隔
@@ -88,15 +88,15 @@ export class stplus extends plugin {
           for (let i = 0; i < [num]; i++) {
               let url = Math.floor(Math.random() * 5) + 1;
               if (url === 1) {
-                  url = `https://www.acy.moe/api/r18`;
+                  url = `http://api.xemxem.ml/API/setu.php`;
               } else if (url === 2) {
-                  url = `https://www.acy.moe/api/r18`;
+                  url = `http://api.xemxem.ml/API/setu.php`;
               } else if (url === 4) {
                   url = `https://moe.jitsu.top/api/?sort=r18&size=small&type=302`;
               } else if (url === 3) {
-                  url = `https://www.acy.moe/api/r18`
+                  url = `http://api.xemxem.ml/API/setu.php`
               } else {
-                  url = `https://www.acy.moe/api/r18`
+                  url = `https://moe.jitsu.top/api/?sort=r18&size=small&type=302`
               }
         let msg = [segment.image(url)]
         image.push(msg)
@@ -113,15 +113,15 @@ export class stplus extends plugin {
           for (let i = 0; i < [num]; i++) {
               let url = Math.floor(Math.random() * 5) + 1;
               if (url === 1) {
-                  url = `https://www.acy.moe/api/r18`;
+                  url = `http://api.xemxem.ml/API/setu.php`;
               } else if (url === 2) {
-                  url = `https://www.acy.moe/api/r18`;
+                  url = `http://api.xemxem.ml/API/setu.php`;
               } else if (url === 3) {
                   url = `https://moe.jitsu.top/api/?sort=r18&size=small&type=302`;
               } else if (url === 4) {
-                  url = `https://www.acy.moe/api/r18`
+                  url = `http://api.xemxem.ml/API/setu.php`
               } else {
-                  url = `https://www.acy.moe/api/r18`
+                  url = `https://moe.jitsu.top/api/?sort=r18&size=small&type=302`
               }
         let msg = [segment.image(url)]
         let abc =  await e.reply(msg,false,{recallMsg:0})//私聊撤回间隔
